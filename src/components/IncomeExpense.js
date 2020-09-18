555cd24f0cd383ba1ea230fe14b4de55f5edb372
kana-wwib <mukrabwy103@gmail.com>
2020-09-19 08:10:24 +0900

    %TOPパート完了

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -14,21 +14,24 @@ export const IncomeExpense = ({ incomeItems, expenseItems }) => {
     if (incomeTotal >= 1) {
       return `${Math.round((expenseTotal / incomeTotal) * 100)} %`;
     } else {
-      return '--';
+      return '---';
     }
   };
   
-
   return (
     <div className="inc-exp-container">
       <div className="inc-container">
         <h2>収入</h2>
-        <p className="income-total">+ {Number(incomeTotal).toLocaleString()}<span> 円</span></p>
+        <div className="right">
+          <p className="income-total">+ {Number(incomeTotal).toLocaleString()}<span> 円</span></p>
       </div>
+        </div>
       <div className="exp-container">
         <h2>支出</h2>
-        <p className="expense-total">- {Number(expenseTotal).toLocaleString()}<span> 円</span></p>
-        <div className="totalPercentage">{percentage()}</div>
+        <div className="right">
+          <p className="expense-total">- {Number(expenseTotal).toLocaleString()}<span> 円</span></p>
+          <div className="totalPercentage">{percentage()}</div>
+        </div>
       </div>
     </div>
   )
